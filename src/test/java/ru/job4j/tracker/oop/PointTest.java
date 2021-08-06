fb2e82fc42f8eb1667f5adba92607197e11d7098
sklopkov <aeol79@gmail.com>
2021-08-06 14:08:59 +0300

    2.2. Рефакторинг - Площадь треугольника. [#122642]

---
+++ src/test/java/ru/job4j/tracker/oop/PointTest.java
@@ -0,0 +1,45 @@
+package ru.job4j.tracker.oop;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class PointTest {
+    Point point1;
+    Point point2;
+
+    @Test
+    public void when00to20then2() {
+        double expected = 2;
+        point1 = new Point(0, 0);
+        point2 = new Point(2, 0);
+        double out = point1.distance(point2);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    public void when20to00then2() {
+        double expected = 2;
+        point1 = new Point(2, 0);
+        point2 = new Point(0, 0);
+        double out = point1.distance(point2);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    public void when13toMinus1Minus3then6dot32() {
+        double expected = 6.32;
+        point1 = new Point(1, 3);
+        point2 = new Point(-1, -3);
+        double out = point1.distance(point2);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    public void when60to06then8dot48() {
+        double expected = 8.48;
+        point1 = new Point(6, 0);
+        point2 = new Point(0, 6);
+        double out = point1.distance(point2);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+}
