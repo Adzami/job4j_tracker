a001bb449c814d11788d276063bb7d911af4390c
sklopkov <aeol79@gmail.com>
2021-08-06 18:04:30 +0300

    2. Модель данных. [#174683]

---
+++ src/main/java/ru/job4j/tracker/pojo/Student.java
@@ -0,0 +1,33 @@
+package ru.job4j.tracker.pojo;
+
+import java.time.LocalDateTime;
+
+public class Student {
+    private String fio;
+    private String group;
+    private LocalDateTime date;
+
+    public String getFio() {
+        return fio;
+    }
+
+    public void setFio(String fio) {
+        this.fio = fio;
+    }
+
+    public String getGroup() {
+        return group;
+    }
+
+    public void setGroup(String group) {
+        this.group = group;
+    }
+
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDateTime date) {
+        this.date = date;
+    }
+}
