98e46e1661d8df00fe9265e368e2874430df1a9e
sklopkov <aeol79@gmail.com>
2021-08-23 14:30:53 +0300

    1. Паспорт и жители [#212656 #193421]

--- src/test/java/ru/job4j/collection/PassportOfficeTest.java
+++ src/test/java/ru/job4j/collection/PassportOfficeTest.java
@@ -6,12 +6,20 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
 public class PassportOfficeTest {
-
     @Test
     public void add() {
         Citizen citizen = new Citizen("2f44a", "Petr Arsentev");
         PassportOffice office = new PassportOffice();
         office.add(citizen);
         assertThat(office.get(citizen.getPassport()), is(citizen));
+    }
+
+    @Test
+    public void addSamePassport() {
+        Citizen citizen1 = new Citizen("2f44a", "Petr Arsentev");
+        Citizen citizen2 = new Citizen("2f44a", "Vasiliy Ivanov");
+        PassportOffice office = new PassportOffice();
+        office.add(citizen1);
+        assertFalse(office.add(citizen2));
     }
 }
