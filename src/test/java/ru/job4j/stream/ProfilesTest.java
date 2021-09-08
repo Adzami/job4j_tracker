93300a8400f8016dbaa6dee3dd6f74a57237ecf4
sklopkov <aeol79@gmail.com>
2021-09-08 15:18:50 +0300

    3. Уникальность элементов и сортировка. [#110225]

--- src/test/java/ru/job4j/stream/ProfilesTest.java
+++ src/test/java/ru/job4j/stream/ProfilesTest.java
@@ -4,7 +4,6 @@ import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.function.Predicate;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
@@ -14,6 +13,8 @@ public class ProfilesTest {
     public void whenCollectAddresses() {
         List<Profile> profiles = List.of(
                 new Profile(new Address("Moscow", "Krasnaya ploshad", 1, 1)),
+                new Profile(new Address("Moscow", "Krasnaya ploshad", 1, 1)),
+                new Profile(new Address("Moscow", "Krasnaya ploshad", 2, 1)),
                 new Profile(new Address("St. Peterburg", "Dvorcovaya ploshad", 10, 10)),
                 new Profile(new Address("Vladimir", "Lenina st.", 11, 11)),
                 new Profile(new Address("Vladivostok", "Portovaya st.", 2, 2))
@@ -22,6 +23,7 @@ public class ProfilesTest {
         List<Address> rsl = pr.collect(profiles);
         List<Address> expected = new ArrayList<>();
         expected.add(new Address("Moscow", "Krasnaya ploshad", 1, 1));
+        expected.add(new Address("Moscow", "Krasnaya ploshad", 2, 1));
         expected.add(new Address("St. Peterburg", "Dvorcovaya ploshad", 10, 10));
         expected.add(new Address("Vladimir", "Lenina st.", 11, 11));
         expected.add(new Address("Vladivostok", "Portovaya st.", 2, 2));
